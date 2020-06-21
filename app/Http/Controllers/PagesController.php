08b2ff82f19880946977d235fe287d68c219584b
Micki <gsmhirsch@gmail.com>
2020-06-21 02:52:33 +0200

    layout, passing values

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -8,14 +8,22 @@ class PagesController extends Controller
 {
     public function index()
     {
-        return view('pages.index');
+        $title = 'Welcome';
+        // return view('pages.index', compact('title'));
+        return view('pages.index')->with('title', $title);
+
     }
     public function about()
     {
-        return view('pages.about');
+        $title = 'About us';
+        return view('pages.about')->with('title', $title);
     }
     public function services()
     {
-        return view('pages.services');
+        $data = array(
+            'title' => 'Services',
+            'services' => ['Web Design', 'Programming']
+        );
+        return view('pages.services')->with($data);
     }
 }
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,13 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>{{config('app.name')}}</title>
-    </head>
-    <body>
-       <h1>About</h1>
-       <p>This is the About</p>
-    </body>
-</html>
+@section('content')
+    <h1><?php echo $title ?></h1>
+    <p>This is the About</p>
+@endsection
+
+
